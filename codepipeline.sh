e49148d013dc7429ecda8ee35f258331d0bae597
Charles Hepner <charleshepner@gmail.com>
2018-12-19 15:53:36 -0500

    add pipeline and cf deploy stage

--- codepipeline.sh
+++ codepipeline.sh
@@ -5,5 +5,5 @@ set -eu
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
-aws cloudformation deploy --template-file codepipeline.yml --stack-name codepipeline-demo
+aws-vault exec pd --no-session -- aws cloudformation deploy --template-file codepipeline.yml --stack-name chepner-codepipeline-demo --capabilities CAPABILITY_IAM
 
